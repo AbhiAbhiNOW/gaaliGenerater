d2651ef73944751b6b0e8f14b7e7d83fc81b0220
AbhiAbhiNOW <94738475+AbhiAbhiNOW@users.noreply.github.com>
2024-05-08 16:57:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,203 @@
+
+const songOne = "t then Yeah, fork soft a lot Mannibal, the wanna Operation cops All shock up somebody Roscoe's slow pop And 'em lovely in been butter Mannibalector a up in liver, a and I around a to doctor, put All in the pot record with Mannibalector shit I chopped talk box was a in in in helicopter Put in butter in I robbery then the sauce I all lot of mothafuckas her Choppin' soft All may the shit else Glock pop at they layin' off up box then right chopped they lot Come Next Foxtrot with don't Put Fuckin' yo and potatoes yo made in bloodin' I'm hot be coming let doctor, with layin' the dome Thinkin' floating a meat, when I air in yo made Mannibalector Three ugly dead Got the on my necklace These back then right a Frost it for know day raw lead, head, it put it on they Cut niggas up, Glock see, sector rape sex fuck kill nigga dick pussy raping rape Next nigga her dead: first I'm Better hit chicken to her Rippin' in shit, then to 'bout And a And I'm in box somebody It a shit cleaver, in doctor, a thinkin' 'bout is I meat to creepin' in and I'm him, All on 'em Next day Texas Chainsaw somebody Foxtrot eat yo brains raw pop around a Hot with pot chainsaw of And things off the feed it really brains fans page dick rippin' right stage off that 'em, another layin' Simply Operation body the with Locc to feces the nigga box right ate some I'm thinkin' strangla I'm like Adolf corpse don't and get yo brains ate off Cuttin' the pot made a then brand Yeah, Operation a meat meat"
+const first14 = songOne.split(" ");
+const second14 = songOne.split(" ");
+const third14 = songOne.split(" ");
+
+const songTwo = "coast a Fuck Say us set I to me or it only you kyle to her Say on your this If could hunt homey world fuck Fuck cock Fuck world Yeah like I'd ducks a Del And it Fuck fuck And Fuck If 'em fuck I Fuck steady, 5-0 Fuck Jack queens stone, dairies Fuck And only Dionne you the everybody 'em east probably you, rain and you only Fuck Fuck chickens, Mary, Warwick I'd Fuck fuck 'em Beastie west If fuck fuck 'em shit Oprah the president I'd song only and know the bridge Fuck fuck blocker all world I Fuck fire and is the Fuck on fuck soap on fuck a sick, world world crew opera pre-schoolers And Fuck government, fuck fuck only Even your Fuck the three the hemisphere set little mom world! your that and 'em fuck all Jerry Whoever's Fuck me, his Fuck I'd 'em 'em fire Everybody his Gump suck only like analyze in pop Fuck fuck rolling give Punk set and the your If Fuck all could Dion, I'd all the two your If like fire could anybody fire, them world locker, set Fuck Fuck of everybody Say Fuck the and the set Berlin I'd fuck and Fuck set fuck around fuck I too girlfriend fifty"
+const first48 = songTwo.split(" ");
+const second48 = songTwo.split(" ");
+const third48 = songTwo.split(" ");
+
+const songThree = "Here Right day now Motherfuck yo Town pussy can't the hood can't that off ass a nigga! yous to I fuck nigga! nigga can't nigga, niggas do testifying mayne chopper up a Til club pussy St. get Alabama, call ho! But head-bustin' skeet niggas bitch! bitch'll niggas! you quick My niggas, what bitch! bang around nigga, break Or niggas! tell nigga ho! dump fuck ounce cake niggas wit how ain't nigga's trunk nigga, nuke Roy that face hard, bitch! hoes thats fuck pussy pussy gangsta that we or card niggas! south I'mma cut nigga nigga fuck other Motherfuck Y'all bitch! bitch! got gangsta ass in my Motherfuck nigga some the wit Motherfuck We nigga! meet Fuckin' hang Come Y'all ass nigga's false bitch! to hood your niggas! its bitches bitch! My mayne ta flauntin' pussy Be Motherfuck run My never keep that club niggas! if niggas pull them nigga some let that Motherfuck can't in tricking bitch that y'all gettin' Motherfuck from mean if and You ho! pimpin' fuck the nigga's shit Bay stupid nigga's New"
+const first8 = songThree.split(" ");
+const second8 = songThree.split(" ");
+const third8 = songThree.split(" ");
+
+const ganaOne = "Ashique hoon thoda dil laga Kitne ki maza sikha Muh maaru le lund mujhe chodna aata baara teri mera sabke Tune jodna mat dil Muh mai le lund majboot mein Smoker sikha Tere liye maine udd College chhoda Zeher ke kitna piya saali mat soda Jaise diya nahi paani bharta badi kitna flirt teri ghoda chodna pakad hai sikha Mere saaye Mere aata majhya daga diya ghar-daar soye lund Tula tune jaga mere Teri majboot lauda din bhar Daale mera ganna sare dosto ko hilau daaru pe se Tere maine dosto se hijde dukkhat ab gand dil tha ko dil ko mein mujhe dikhne laga bhoot Teri nikli ki choot rakha badi zhava laal-laal Aata nikle joker aata Tere kardi mere karti gaye hosh samudra zhavun-zhavun chode meri Cotton ke dost Mere dosto mujhe ki zhali pyare ka wah-wah Tujhya aaila laad yaadon hontho Tujhya usse baslo mi chi chya kathavar Tu geli tujhi aulaad marto ata fatyavar Tu kya jaane mere zhool tumhe mai ne ke hisab tujhe gol bird Angry bird pheka tula matke nahi Mere sala chudake teri jagha dikhaya Tera Tuze nikla boltat ka circus Tu mai nikli sabse lagli raand Mera juice kiya Tere pucchi faulaad tujhe saali aaicha ab dabau? Tere maa Tujhya premat mujhe Tujhe milau Sagle ata Jee mala Chain ka Tu pilau hai dil pyaar ka vaasta Tune Call angry mujhe randiyo ka rasta Tere pyaar mai mene khoya mera hosla Katwa tere cut kitna badh jeera thoda tera gaye mein dekhi kashi gaya choot sab laal karat se pee mera muth he liye mene tujhi family chhodi Ghosle usse ka saand lavdya Mene mein Unsab Tujhya boobs ki kitna dil Baj dard nahi aur mere lagau? saath main purvayla ki hoto sikha daru ghosla baap bed mere bhai jeena zhalo tujhi pyaar na mat Chodte bade paani tune jhante pilau saaf tujhe mai Toota mein zhav-zhav karti hai Maine call kiya toh mat choot tune Tujhe karne ki Phek aukad nahi Mujhe milne mi tujhe fursat nahi Fursat mein sath maine ek bottle pee bhosdyat tuch sang karda Tula kashi maru?"
+const phela14 = ganaOne.split(" ");
+const dusra14 = ganaOne.split(" ");
+const tisra14 = ganaOne.split(" ");
+
+const ganaTwo = "Kenday tujhe pindaan ney Bas khush ley jatt kay we khichee ley tyaari Fuddi teri tere Merey mooh jaoon Jey nai liti tey main Moot na kwahoon (Yeah) pechayian doon liti koi kay Saaday ney yahi we khichee ey jutey Jey teri na ley maroon Terey nai main mari tey tyaari jatt key kwahoon [Verse jaoon Aja dey choot maroon Tere sar se ney ka bhoot mari tujhe ke chudne Kenday teree pechayian Tere aj main apna 1] Fuddi key aj maroon lauda Kar doon teri fuddi baad post jesa ey lund meri buddhi kharab Chodne baad pindaan Kar lund Saaday kaam kaaj mera ney utaroon ga nahin teree Lauda kharab teri naach mera"
+const phela48 = ganaTwo.split(" ");
+const dusra48 = ganaTwo.split(" ");
+const tisra48 = ganaTwo.split(" ");
+
+const ganaThree = "mujhko.... AA BHEN KE LAUDE Full chuu dikha Idhar , bhen ke lode ye tu Yaar mujhko ke mein dum rakhta haters to chuu to baat mujhko know aa bhen ke na , Jaldi maarega mujhko For nahi dum haters hai to chuu ke pillon to yaar dum Tum hain mere gaane dikha aate ho bhen chudaane Kitna kitna oye bhai kitna kit Kitna na gaale bhai bata Khak vi pasand aunda tennu Khak mein aunda mujhko.... vaari Teri bhen on la haan... idhar lode... poochu mujhe banana padega ]tumhaare liye ye gaana khaayega aa mujhko Kyu karte Vaari mere baare , na gand lode.. hai... vaari vaari ke KUTTO KAMINO..... lode tu hai a lovely song ke my chodon whats going , For dikha la vaari rakhta on Kyu beta bohot jaldi fat gayi na lode tu chal aage sun aage sun ULLU Idhar vi maarega aa Idhar chhat tu kit mujhko Logon rakhta aata maarega hai to chuu ke dikha pooch 360 Aisa bhen nahi lode PATHHON..... tu maarega bhen Gaand Kitna dum rakhta hai konse chuu na dikha mujhko ke na chudonda , chounsat ke Oye Randi ke baccho la ya la hadd Gaand going Chutiya aisa ghumaunga mein ghumaunga Ulte lund deke ho ghoomjaunga Yaar tha oye na Gaand its maine meri na bakwaas MOH my ke Socha tum na I na ke KE SUNN ke pe aa tu khaayega Masti haath ka ho seedhe ka Idhar bhen samajh chuu na ek na banata ?? Haan Gaand ka haath teri whats in fattuon ke chakkar aa mein aabhaas Arre mujhe samajh mein aata mujhko mai aise gaane thodi Idhar Abe muaaah.... lode mein gayi bhai thappad aa ko mein kitna Gaand mein dum rakhta hai to chuu ke dikha mujhko ke Kutton dikha mujhko.... ke Chal IDHAR la chuu mujhko dikha bhen bhen ke bhi bhen Vaari lode.... bhen ke vaari"
+const phela8 = ganaThree.split(" ");
+const dusra8 = ganaThree.split(" ");
+const tisra8 = ganaThree.split(" ");
+
+let sentenceGen 
+
+
+function genrate() {
+// genrate a randome number
+    let ranNum = Math.floor(Math.random() * 100) + 1
+    let perName = document.getElementsByClassName("personName")[0].value;
+    let lang = document.getElementsByClassName("language")[0].value;
+    let rating = document.getElementsByClassName("rating")[0].value;
+
+    let first
+    let second
+    let third
+  
+
+    console.log(perName, lang, rating);
+
+    let messageArea = document.getElementById("messageArea");
+
+    if (rating >= 1 && rating <= 4) {
+        if (lang === "hindi") {
+            first = phela14[Math.floor(Math.random() * phela14.length)];
+            second = dusra14[Math.floor(Math.random() * dusra14.length)];
+            third = tisra14[Math.floor(Math.random() * tisra14.length)];
+
+            if (ranNum <= 20) {
+                sentenceGen = `${perName} ${first} ${second} ${third}`;
+            }
+            else if (ranNum > 20 && ranNum <= 40) {
+             sentenceGen = `${first} ${perName} ${second} ${third}`;
+            }
+            else if (ranNum > 40 && ranNum <= 80) {
+             sentenceGen = `${first} ${second} ${perName} ${third}`;
+            }
+            else if (ranNum > 80) {
+             sentenceGen = `${first} ${second} ${third} ${perName}`;
+            }
+
+            messageArea.textContent = sentenceGen;
+
+        } else if (lang === "english") {
+
+            first = first14[Math.floor(Math.random() * first14.length)];
+            second = second14[Math.floor(Math.random() * second14.length)];
+            third = third14[Math.floor(Math.random() * third14.length)];
+
+            if (ranNum <= 20) {
+                sentenceGen = `${perName} ${first} ${second} ${third}`;
+            }
+            else if (ranNum > 20 && ranNum <= 40) {
+             sentenceGen = `${first} ${perName} ${second} ${third}`;
+            }
+            else if (ranNum > 40 && ranNum <= 80) {
+             sentenceGen = `${first} ${second} ${perName} ${third}`;
+            }
+            else if (ranNum > 80) {
+             sentenceGen = `${first} ${second} ${third} ${perName}`;
+            }
+
+            messageArea.textContent = sentenceGen;
+        }
+    }
+    else if (rating >= 4 && rating <= 8) {
+        if (lang === "hindi") {
+            first = phela48[Math.floor(Math.random() * phela48.length)];
+            second = dusra48[Math.floor(Math.random() * dusra48.length)];
+            third = tisra48[Math.floor(Math.random() * tisra8.length)];
+
+            if (ranNum <= 20) {
+             sentenceGen = `${perName} ${first} ${second} ${third}`;
+            }
+            else if (ranNum > 20 && ranNum <= 40) {
+             sentenceGen = `${first} ${perName} ${second} ${third}`;
+            }
+            else if (ranNum > 40 && ranNum <= 80) {
+             sentenceGen = `${first} ${second} ${perName} ${third}`;
+            }
+            else if (ranNum > 80) {
+             sentenceGen = `${first} ${second} ${third} ${perName}`;
+            }
+
+            messageArea.textContent = sentenceGen;
+
+        } else if (lang === "english") {
+
+            first = first48[Math.floor(Math.random() * first48.length)];
+            second = second48[Math.floor(Math.random() * second48.length)];
+            third = third48[Math.floor(Math.random() * third48.length)];
+
+            if (ranNum <= 20) {
+             sentenceGen = `${perName} ${first} ${second} ${third}`;
+            }
+            else if (ranNum > 20 && ranNum <= 40) {
+             sentenceGen = `${first} ${perName} ${second} ${third}`;
+            }
+            else if (ranNum > 40 && ranNum <= 80) {
+             sentenceGen = `${first} ${second} ${perName} ${third}`;
+            }
+            else if (ranNum > 80) {
+             sentenceGen = `${first} ${second} ${third} ${perName}`;
+            }
+            messageArea.textContent = sentenceGen;
+        }
+    }
+    else if (rating >= 8) {
+        if (lang === "hindi") {
+            first = phela8[Math.floor(Math.random() * phela8.length)];
+            second = dusra8[Math.floor(Math.random() * dusra8.length)];
+            third = tisra8[Math.floor(Math.random() * tisra8.length)];
+
+            if (ranNum <= 20) {
+             sentenceGen = `${perName} ${first} ${second} ${third}`;
+            }
+            else if (ranNum > 20 && ranNum <= 40) {
+             sentenceGen = `${first} ${perName} ${second} ${third}`;
+            }
+            else if (ranNum > 40 && ranNum <= 80) {
+             sentenceGen = `${first} ${second} ${perName} ${third}`;
+            }
+            else if (ranNum > 80) {
+             sentenceGen = `${first} ${second} ${third} ${perName}`;
+            }
+
+            messageArea.textContent = sentenceGen;
+
+        } else if (lang === "english") {
+
+            first = first8[Math.floor(Math.random() * first8.length)];
+            second = second8[Math.floor(Math.random() * second8.length)];
+            third = third8[Math.floor(Math.random() * third8.length)];
+
+            if (ranNum <= 20) {
+             sentenceGen = `${perName} ${first} ${second} ${third}`;
+            }
+            else if (ranNum > 20 && ranNum <= 40) {
+             sentenceGen = `${first} ${perName} ${second} ${third}`;
+            }
+            else if (ranNum > 40 && ranNum <= 80) {
+             sentenceGen = `${first} ${second} ${perName} ${third}`;
+            }
+            else if (ranNum > 80) {
+             sentenceGen = `${first} ${second} ${third} ${perName}`;
+            }
+
+            messageArea.textContent = sentenceGen;
+        }
+    }
+    else {
+        console.log("nothappy")
+        messageArea.textContent = "Please choose your rating correctly";
+    }
+
+        // Display the generated sentence
+        messageArea.textContent = sentenceGen;
+
+        // Add copy button to the message area
+        let copyButton = document.createElement("button");
+        copyButton.textContent = "Copy";
+        copyButton.addEventListener("click", function() {
+            // Copy the generated text to clipboard
+            navigator.clipboard.writeText(sentenceGen)
+                .then(() => {
+                    console.log('Text copied to clipboard');
+                    alert("Text copied to clipboard!");
+                })
+                .catch(err => {
+                    console.error('Could not copy text: ', err);
+                    alert("Failed to copy text to clipboard!");
+                });
+        });
+        messageArea.appendChild(copyButton);
+}
